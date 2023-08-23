5c92fb950882809d4b8c8d66d5ff4cef995420f0
syed.n <syed.n@marketsimplified.com>
2023-08-24 02:40:10 +0530

    first

--- src/main/kotlin/com/example/entity/SchemeTable.kt
+++ src/main/kotlin/com/example/entity/SchemeTable.kt
@@ -1,8 +1,8 @@
 package com.example.entity
 
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.Table
 
-object SchemeTable:UUIDTable(name = "scheme_table"){
+object SchemeTable: Table(name = "scheme_table"){
     val schemeCode = varchar(name = "scheme_code", length = 256)
     val schemeName = varchar(name = "scheme_name", length = 256)
 }
